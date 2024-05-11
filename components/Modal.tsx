9e61da9204b2f9a3c7e1a5a415215697397e131a
HrishikShaji <hrishikminishaji@gmail.com>
2024-05-11 16:12:03 +0530

    rendering report done

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -11,6 +11,7 @@ export const Modal = () => {
   const { getNode } = useReactFlow();
   const [currentNode, setCurrentNode] = useState<Node | null>(null);
   const [report, setReport] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const node = getNode(id);
@@ -20,44 +21,56 @@ export const Modal = () => {
   if (!isOpen) return null;
 
   async function generateReport() {
-    const response = await fetch(
-      "https://pvanand-generate-subtopics.hf.space/generate_report",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          topic: currentNode.data.label,
-          description: currentNode?.data.desc,
-        }),
-      },
-    );
-
-    const result = await response.json();
-    console.log(result);
-    setReport(result.report);
+    try {
+      setLoading(true);
+      const response = await fetch(
+        "https://pvanand-generate-subtopics.hf.space/generate_report",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            topic: currentNode.data.label,
+            description: currentNode?.data.desc,
+          }),
+        },
+      );
+      const result = await response.json();
+      console.log(result);
+      setReport(result.report);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
-    <div className="h-screen w-full flex sm:p-0 p-10 items-center z-50 bg-black/70 justify-center fixed top-0 left-0 rounded-md ">
-      <div className="p-10  relative flex flex-col items-center justify-center gap-10 rounded-3xl bg-neutral-600">
+    <div className="h-screen w-full flex sm:p-0  items-center z-50 bg-black/70 justify-center fixed top-0 left-0 rounded-md ">
+      <div className="p-10  relative flex w-[70vw] flex-col items-center justify-center gap-2 rounded-3xl bg-neutral-600">
         <button
           onClick={() => {
             closeModal();
+            setCurrentNode(null);
+            setReport("");
           }}
-          className="absolute top-2 right-2 "
+          className="absolute top-4 right-4 rounded-md bg-black text-white p-2"
         >
           close
         </button>
+        <div className="flex flex-col gap-2 text-white">
+          <h1 className="font-semibold text-xl">{currentNode?.data.label}</h1>
+          <h1>{currentNode?.data.desc}</h1>
+        </div>
+        <div
+          className="h-[30vh] w-full overflow-y-scroll"
+          dangerouslySetInnerHTML={{ __html: report }}
+        ></div>
         <button
           className="p-2 bg-black text-white rounded-md"
           onClick={generateReport}
         >
-          Generate Report
+          {loading ? "Generating Report..." : "Generate Report"}
         </button>
-        <div className="flex flex-col gap-10 text-white">
-          {currentNode?.data.label}
-        </div>
-        {report}
       </div>
     </div>
   );
