adc7b6a6300f3c9f1f9743aaf5f4885c33e5a2c7
HrishikShaji <hrishikminishaji@gmail.com>
2024-05-11 15:50:38 +0530

    report generation done

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,37 +3,62 @@ import { useModal } from "@/context/ModalContext";
 import { Node, useReactFlow } from "reactflow";
 
 type LookupType = {
-	[key: string]: ReactNode;
+  [key: string]: ReactNode;
 };
 
 export const Modal = () => {
-	const { isOpen, closeModal, id } = useModal();
-	const { getNode } = useReactFlow();
-	const [currentNode, setCurrentNode] = useState<Node | null>(null);
-	useEffect(() => {
-		const node = getNode(id);
-		if (!node) return;
-		setCurrentNode(node);
-	}, [id]);
-	if (!isOpen) return null;
+  const { isOpen, closeModal, id } = useModal();
+  const { getNode } = useReactFlow();
+  const [currentNode, setCurrentNode] = useState<Node | null>(null);
+  const [report, setReport] = useState("");
 
-	async function generateReport() { }
+  useEffect(() => {
+    const node = getNode(id);
+    if (!node) return;
+    setCurrentNode(node);
+  }, [id]);
+  if (!isOpen) return null;
 
-	return (
-		<div className="h-screen w-full flex sm:p-0 p-10 items-center z-50 bg-black/70 justify-center fixed top-0 left-0 rounded-md ">
-			<div className="p-10  relative flex flex-col items-center justify-center gap-10 rounded-3xl bg-neutral-600">
-				<button
-					onClick={() => {
-						closeModal();
-					}}
-					className="absolute top-2 right-2 "
-				>
-					close
-				</button>
-				<div className="flex flex-col gap-10 text-white">
-					{currentNode?.data.label}
-				</div>
-			</div>
-		</div>
-	);
+  async function generateReport() {
+    const response = await fetch(
+      "https://pvanand-generate-subtopics.hf.space/generate_report",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          topic: currentNode.data.label,
+          description: currentNode?.data.desc,
+        }),
+      },
+    );
+
+    const result = await response.json();
+    console.log(result);
+    setReport(result.report);
+  }
+
+  return (
+    <div className="h-screen w-full flex sm:p-0 p-10 items-center z-50 bg-black/70 justify-center fixed top-0 left-0 rounded-md ">
+      <div className="p-10  relative flex flex-col items-center justify-center gap-10 rounded-3xl bg-neutral-600">
+        <button
+          onClick={() => {
+            closeModal();
+          }}
+          className="absolute top-2 right-2 "
+        >
+          close
+        </button>
+        <button
+          className="p-2 bg-black text-white rounded-md"
+          onClick={generateReport}
+        >
+          Generate Report
+        </button>
+        <div className="flex flex-col gap-10 text-white">
+          {currentNode?.data.label}
+        </div>
+        {report}
+      </div>
+    </div>
+  );
 };
